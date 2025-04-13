30727d9bb0150366d08cb13973d95671532c9eb2
Huy Cao <caogiahuy615@gmail.com>
2025-04-13 15:52:28 -0400

    Setup kafka and flink

---
+++ back_end/feature_store/stream_processing/consumer.py
@@ -0,0 +1,41 @@
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.common.serialization import DeserializationSchema, SimpleStringSchema
+from pyflink.common.typeinfo import Types
+from pyflink.common import WatermarkStrategy
+from pyflink.datastream.connectors.kafka import FlinkKafkaConsumer
+
+import json
+import os
+
+env = StreamExecutionEnvironment.get_execution_environment()
+
+# Path to JAR files
+jars_dir = "/Users/huybro/Desktop/feature_store_fraud_detection/jars/" #Modify to your directory
+
+# Add the Kafka connector JAR with proper file:// protocol
+jar_path = f"file://{os.path.abspath(os.path.join(jars_dir, 'flink-connector-kafka-3.3.0-1.20.jar'))}" 
+env.add_jars(jar_path)
+
+kafka_clients_jar = f"file://{os.path.abspath(os.path.join(jars_dir, 'kafka-clients-7.9.0-ccs.jar'))}"
+env.add_jars(kafka_clients_jar)
+
+# Create a Kafka source
+properties = {
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'pyflink-consumer'
+}
+
+stream = env.add_source(
+    FlinkKafkaConsumer(
+        topics='transactions',
+        deserialization_schema=SimpleStringSchema(),
+        properties=properties
+    )
+)
+
+# Parse JSON
+parsed = stream.map(lambda raw: json.loads(raw), output_type=Types.MAP(Types.STRING(), Types.STRING()))
+parsed.print()
+
+# Execute the stream processing pipeline
+env.execute("PyFlink Kafka Stream")
---
+++ back_end/feature_store/stream_processing/producer.py
@@ -0,0 +1,36 @@
+from kafka import KafkaProducer
+import json
+import time
+import random
+from datetime import datetime
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+def generate_fake_transaction():
+    raw_data = {
+        "transaction_id": random.randint(100000, 999999),
+        "amount": round(random.uniform(10, 500), 2),
+        "merch_long": random.randint(-130,130),
+        "merch_lat": random.randint(-130,130),
+        "long": random.randint(-130,130),
+        "lat": random.randint(-130,130),
+        "timestamp": datetime.now().isoformat()
+    }
+    return {k: str(v) for k, v in raw_data.items()}
+
+if __name__ == "__main__":
+    print("🚀 Sending fake transactions to Kafka topic 'transactions'...")
+    while True:
+        txn = generate_fake_transaction()
+        print(f"Generated: {txn}")
+
+        try:
+            producer.send('transactions', txn).get(timeout=10)
+            print("✔ Sent to Kafka")
+        except Exception as e:
+            print(f"❌ Failed to send: {e}")
+
+        time.sleep(1)
