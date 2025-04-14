4fe5bbae974ce11c8535d40d5e3d34f59b5ad79e
dmkhang1101 <114703634+dmkhang1101@users.noreply.github.com>
2025-04-13 23:12:09 -0400

    Update consumer.py

--- back_end/feature_store/stream_processing/consumer.py
+++ back_end/feature_store/stream_processing/consumer.py
@@ -6,10 +6,38 @@ from pyflink.datastream.functions import ProcessWindowFunction, RuntimeContext
 from pyflink.datastream.window import TumblingEventTimeWindows, SlidingEventTimeWindows
 from pyflink.datastream.state import ValueStateDescriptor
 from pyflink.common.watermark_strategy import TimestampAssigner
+from pyflink.datastream.window import SlidingProcessingTimeWindows
 from math import radians, sin, cos, sqrt, atan2
+from typing import Iterable
 import datetime
 import json
 import os
+
+class TransactionTimestampAssigner(TimestampAssigner):
+    def extract_timestamp(self, element, record_timestamp):
+        timestamp = int(datetime.datetime.fromisoformat(element['timestamp']).timestamp() * 1000)
+        return timestamp
+
+class TxnCountLast10Min(ProcessWindowFunction):
+    def process(self, key, context: ProcessWindowFunction.Context, elements: Iterable[dict]):
+        elements_list = list(elements)
+        count = len(elements_list)
+        yield({'txn_count': str(count)})
+
+class AvgAmtLast1Hour(ProcessWindowFunction):
+    def process(self, key, context: ProcessWindowFunction.Context, elements: Iterable[dict]):
+        elements_list = list(elements)
+        amounts = [float(e['amount']) for e in elements_list]
+        avg = sum(amounts) / len(amounts)
+        yield({'avg_amt_last_1_hour': str(avg)})
+
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371  # Earth radius in km
+    lat1, lon1, lat2, lon2 = map(float, [lat1, lon1, lat2, lon2])
+    dlat = radians(lat2 - lat1)
+    dlon = radians(lon2 - lon1)
+    a = sin(dlat / 2) ** 2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon / 2) ** 2
+    return R * 2 * atan2(sqrt(a), sqrt(1 - a))
 
 env = StreamExecutionEnvironment.get_execution_environment()
 env.set_stream_time_characteristic(TimeCharacteristic.EventTime)
@@ -41,32 +69,6 @@ stream = env.add_source(
 # Parse JSON
 parsed = stream.map(lambda raw: json.loads(raw), output_type=Types.MAP(Types.STRING(), Types.STRING()))
 
-class TransactionTimestampAssigner(TimestampAssigner):
-    def extract_timestamp(self, element, record_timestamp):
-        timestamp = int(datetime.datetime.fromisoformat(element['timestamp']).timestamp() * 1000)
-        return timestamp
-
-class TxnCountLast10Min(ProcessWindowFunction):
-    def process(self, key, context, elements, out):
-        elements_list = list(elements)
-        count = len(elements_list)
-        out.collect({'txn_count': str(count)})
-
-class AvgAmtLast1Hour(ProcessWindowFunction):
-    def process(self, key, context, elements, out):
-        elements_list = list(elements)
-        amounts = [float(e['amount']) for e in elements_list]
-        avg = sum(amounts) / len(amounts)
-        out.collect({'avg_amt_last_1_hour': str(avg)})
-
-def haversine(lat1, lon1, lat2, lon2):
-    R = 6371  # Earth radius in km
-    lat1, lon1, lat2, lon2 = map(float, [lat1, lon1, lat2, lon2])
-    dlat = radians(lat2 - lat1)
-    dlon = radians(lon2 - lon1)
-    a = sin(dlat / 2) ** 2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon / 2) ** 2
-    return R * 2 * atan2(sqrt(a), sqrt(1 - a))
-
 # Assign timestamps and watermarks
 parsed = parsed.assign_timestamps_and_watermarks(
     WatermarkStrategy
@@ -83,21 +85,18 @@ distance_included = parsed.map(
     output_type=Types.MAP(Types.STRING(), Types.STRING())
 )
 
-# Count transactions in the last 10 minutes (sliding every 1 minute)
-txn_count = distance_included.window_all(
-    SlidingEventTimeWindows.of(Time.minutes(10), Time.minutes(1))
-).process(
-    TxnCountLast10Min(),
-    output_type=Types.MAP(Types.STRING(), Types.STRING())
-)
+# Count transactions in the last 10 minutes (sliding every 1 minute) (currently testing with a smaller window of 10 seconds)
+txn_count = distance_included \
+    .key_by(lambda t: t['id']) \
+    .window(SlidingProcessingTimeWindows.of(Time.seconds(10), Time.seconds(1))) \
+    .process(TxnCountLast10Min(),output_type=Types.MAP(Types.STRING(), Types.STRING()))
 
-# Average amount in the last 1 hour (sliding every 1 minute)
-avg_amount = distance_included.window_all(
-    SlidingEventTimeWindows.of(Time.hours(1), Time.minutes(1))
-).process(
-    AvgAmtLast1Hour(),
-    output_type=Types.MAP(Types.STRING(), Types.STRING())
-)
+
+# Average amount in the last 1 hour (sliding every 1 minute) (currently testing with a smaller window of 10 seconds)
+avg_amount = distance_included \
+    .key_by(lambda t: t['id']) \
+    .window(SlidingProcessingTimeWindows.of(Time.seconds(10), Time.seconds(1))) \
+    .process(AvgAmtLast1Hour(),output_type=Types.MAP(Types.STRING(), Types.STRING()))
 
 distance_included.print()
 txn_count.print()
