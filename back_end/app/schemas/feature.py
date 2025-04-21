23968deaec10d23c4b0e9d3275ae9854dfc3fc09
dmkhang1101 <114703634+dmkhang1101@users.noreply.github.com>
2025-04-20 20:45:44 -0400

    add endpoints retrieve offline feature

--- back_end/app/schemas/feature.py
+++ back_end/app/schemas/feature.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class FeatureRow(BaseModel):
     cc_num: int
@@ -13,3 +14,18 @@ class FeatureRow(BaseModel):
     gender: str
     city_pop: int
     is_fraud: int
+
+class FeatureRowRetrieve(BaseModel):
+    cc_num: int
+    amt: float
+    hour_of_day: int
+    day_of_week: int
+    age_at_txn: float
+    distance_to_merchant: float
+    txn_count_last_10_min: int
+    avg_amt_last_1_hour: float
+    category: str
+    gender: str
+    city_pop: int
+    is_fraud: int
+    feature_timestamp: datetime
